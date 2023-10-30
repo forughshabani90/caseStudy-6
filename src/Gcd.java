b23ef7f8b28d7c2c4850da32b9282bade42aef65
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-30 02:19:20 -0700

    second commit

--- src/Gcd.java
+++ src/Gcd.java
@@ -1,11 +1,12 @@
 public class Gcd {
-    public static void gcd(int n1, int n2) {
+    public static int Gcd(int n1, int n2) {
         int k = 2;
         int gcd = 1;
         while (k <= n1 && k <= n2) {
             if (n1 % k == 0 && n2 % k == 0) ;
             gcd = k;
             k++;
+            return gcd;
 
         }
 
--- src/TestVoidMetod.java
+++ src/TestVoidMetod.java
@@ -1,21 +1,25 @@
 public class TestVoidMetod {
     public static void main(String[] args) {
         System.out.println("this grade is : ");
-     PrintGrade(89) ;
+        PrintGrade(89);
+        Gcd gcd = new Gcd();
+       int resul=gcd.Gcd(6,9);
+        System.out.println(resul);
     }
-    public static void PrintGrade(double score){
-      if (score>=90)
-          System.out.println("A");
-      else if (score>=80)
-          System.out.println("B");
-      else if (score>=70)
-          System.out.println("C");
-      {
 
-      }
+    public static void PrintGrade(double score) {
+        if (score >= 90)
+            System.out.println("A");
+        else if (score >= 80)
+            System.out.println("B");
+        else if (score >= 70)
+            System.out.println("C");
         {
 
-      }
+        }
+        {
+
+        }
     }
 }
 
