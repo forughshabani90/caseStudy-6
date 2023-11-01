2974aa33a8ac43fb2bedfdbcd5911457eb6391ad
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-01 04:17:43 -0700

    fc

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -7,7 +7,7 @@ public class PrimeNumber {
     public static boolean primeNambers(int num) {
         int i = 2;
         for (i = 2; i = num / 2; i++) {
-            if (num / i == 0) ;
+            if (num / i == 0)
             return false;
             else
             return true;
