b7c39c9b35fcf9a9883445c584a33ee9a8282457
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-01 04:52:15 -0700

    sc

--- src/OverLoading.java
+++ src/OverLoading.java
@@ -2,8 +2,7 @@ public class OverLoading {
     public static void main(String[] args) {
         System.out.println("the maximum of 3 and 4 is" + max(3, 4));
         System.out.println("the maximum of 3.0 and 4.5 is " + max(3.0, 4.5));
-        System.out.println("the maximum of 3.0 and 4.5 and 6.3 is " + max (3.0 , 4.5, 6.3));
-
+        System.out.println("the maximum of 3.0 and 4.5 and 6.3 is " + max(3.0, 4.5, 6.3));
     }
 
     public static int max(int n1, int n2) {
@@ -20,7 +19,7 @@ public class OverLoading {
         return max;
     }
 
-    public static doublemax(double n1, double n2, double n3) {
+    public static double max(double n1, double n2, double n3) {
         return max(max(n1, n2), n3);
     }
 }
